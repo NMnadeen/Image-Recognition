60a1179763b12ebd34a0be3fb8c86bc62edf84b0
NMnadeen <nyashamacauley@gmail.com>
2025-11-26 16:02:30 +0100

    Oversigt over hvordan koden skal deles op

--- Server/Nadeen/nServer.py
+++ Server/Nadeen/nServer.py
@@ -1,1 +1,7 @@
-#from fastapi import FastAPI
+# Upload image code: https://tutorial101.blogspot.com/2023/02/fastapi-upload-image.html
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import os
+from random import randint
+import uuid
